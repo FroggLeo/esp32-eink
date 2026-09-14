c2e0f23ebb231de4d2cf25525813fc7b88f8f279
Leo <88123479+FroggLeo@users.noreply.github.com>
2026-09-13 20:19:31 -0700

    fix build error and setup display test

--- main/main.cpp
+++ main/main.cpp
@@ -14,19 +14,31 @@ extern "C" void app_main() {
 
     Eink display;
     display.init();
+    display.clear();
 
     Canvas black(128, 296, 90);
     set_font(test_font);
 
+    ESP_LOGI(TAG, "Testing full refresh");
     draw_text(black, 10, 10, "FULL");
     draw_sample(black, 10, 20);
     display.refresh_full(black.get_buffer());
 
+    ESP_LOGI(TAG, "Testing fast refresh");
     draw_text(black, 10, 40, "FAST");
     draw_sample(black, 10, 50);
     display.refresh_fast(black.get_buffer());
 
+    ESP_LOGI(TAG, "Testing partial refresh");
     draw_text(black, 10, 70, "PARTIAL");
     draw_sample(black, 10, 80);
+    draw_text(black, 10, 100, "ERASE");
+    draw_sample(black, 10, 110);
     display.refresh_partial(black.get_buffer());
+
+    ESP_LOGI(TAG, "Testing partial erase");
+    draw_sample(black, 10, 110, true);
+    display.refresh_partial(black.get_buffer());
+
+    ESP_LOGI(TAG, "Testing completed");
 }
